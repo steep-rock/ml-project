aeed23bf1bd94d590ab677d3ca8977bfce97e062
Craig Rudolph <craig.rudolph1101@gmail.com>
2024-02-22 14:55:11 +0200

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 
 # Modelling
-from sklearn.metrics import mean_squared_error, r2_score
+
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor, GradientBoostingRegressor
@@ -43,22 +43,64 @@ class ModelTrainer:
             logging.info('Select the models to be used')
             models = {
                 "Linear Regression": LinearRegression(),
-                "K-Neighbors Regressor": KNeighborsRegressor(),
+                #"K-Neighbors Regressor": KNeighborsRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
                 "Random Forest Regressor": RandomForestRegressor(),
                 "XGBRegressor": XGBRegressor(), 
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
+            
+
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest Regressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
 
             logging.info('Create a report of the models')
-            model_report: dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
-
-            #to get the best model score from dict
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
             
+            #to get the best model score from dict
+            logging.info('From the model report find the best score')
             best_model_score = max(sorted(model_report.values()))
             
             #using the best score, obtain the model name
+            logging.info('Use that best model score to find the name of the best model')
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,10 @@ import numpy as np
 import pandas as pd
 import dill
 import pickle 
+from src.logger import logging
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -20,14 +22,26 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    logging.info('Create an evaluation report of the model')
     try:
         report = {}
 
         for i in range(len(list(models))):
+            logging.info('List of all the models')
             model = list(models.values())[i]
-            #para=param[list(models.keys())[i]]
 
+            logging.info('List of all the parameters')
+            para=param[list(models.keys())[i]]
+            logging.info(str(para))
+            logging.info('Apply grid search for all the parameters')
+            gs = GridSearchCV(model,para,cv=3)
+
+            logging.info('using grid search to fit the models')
+            gs.fit(X_train,y_train)
+
+            logging.info('set the parameters with the best parameters obtained from grid search')
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
